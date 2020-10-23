8097421ac1adb956125ddf966bdb6be64582ab54
tsjuusmei <guusmay2@hotmail.com>
2020-10-23 13:19:17 +0200

    same setup for values

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ function cleanPets() {
             .replace(/[0-9]/g, '')
     })
 
-    // Replacing typos
+    // Replacing typos and plural
     .map((x) => {
         return x
             .replace('hont', 'hond')
             .replace(/dwerg teckel/g, 'hond')
+            .replace('katten', 'kat')
+    })
+
+    // Creating the same setup for values
+    .map((x) => {
+        return x    
+            .replace(/ /g, '')
+            .replace(/,/g, '.')
+            .replace(/:/g, '.')
     })
 
 return pets
