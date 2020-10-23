5acbbc47b621bbd4469ae9c797a9e44b134d60a5
tsjuusmei <guusmay2@hotmail.com>
2020-10-23 11:54:43 +0200

    rendered pets with short code

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const surveyAnswers = data
+function pets() {
+    let pets = data.map(x => x.huisDieren)
 
-console.log(surveyAnswers[0].beroepMoeder)
-
-let kolomNaam = "schoenmaat"
-let lijstAntwoorden = getAnswersForQuestion(surveyAnswers, kolomNaam)
-
-function getAnswersForQuestion(answers, question) {
-
-    if (answers.length < 1) {
-        console.error("Your array has no items in it :(")
-        return
-    }
-
-    return answers.map(answer => answer[question])
-
-    // let answersForQuestion = []
-    // for (answer of answers){
-    //     answersForQuestion.push(answer[question])
-    // }
-    // return answersForQuestion
+    return pets
 }
 
-console.log(lijstAntwoorden)
+console.log(pets())
