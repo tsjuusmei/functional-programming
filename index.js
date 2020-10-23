a6007609cc795f87f390dedc02fcbf42630b2ffc
tsjuusmei <guusmay2@hotmail.com>
2020-10-23 13:01:03 +0200

    emptied empty entries for data

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function cleanPets() {
+    // Clean empty entries
     let pets = data.map(x => {
         return x.huisDieren
             .toLowerCase()
+            .replace('geen', '')
+            .replace(/'/g, '')
+            .replace(/-/g, '')
+            .replace('n>v>t>', '')
     })
 
-    return pets
+return pets
 }
 
 console.log(cleanPets())
