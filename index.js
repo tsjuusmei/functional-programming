247f2185c15dd9f1c53f70be966506587f3110e9
tsjuusmei <guusmay2@hotmail.com>
2020-10-29 23:43:39 +0100

    cleaned up pets column in surveydata

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-// function cleanPets() {
-//     // Clean empty entries
-//     let pets = data.map(x => {
-//         return x.huisDieren
-//             .toLowerCase()
-//             .replace('geen', '')
-//             .replace(/'/g, '')
-//             .replace(/-/g, '')
-//             .replace('n>v>t>', '')
-//             .replace(/[0-9]/g, '')
-//     })
+import { data } from './data/data'
+import * as pets from './assets/pets.json'
 
-//         // Replacing typos and plural
-//         .map((x) => {
-//             return x
-//                 .replace('hont', 'hond')
-//                 .replace(/dwerg teckel/g, 'hond')
-//                 .replace('katten', 'kat')
-//         })
+const animals = pets.default;
 
-//         // Creating the same setup for values
-//         .map((x) => {
-//             return x
-//                 .replace(/ /g, '')
-//                 .replace(/,/g, '.')
-//                 .replace(/:/g, '.')
-//                 .replace('..', '.')
-//         })
-//     return pets
-// }
+function cleanPets() {
 
-// console.log(cleanPets())
-
-// API data ophalen
-
-import { endpoint } from './data';
-
-async function getData(url) {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        let methods = data.map(x => {
-            return x.paymentmethod
+    let cleanPetsData = data
+        .map(x => {
+            return x.huisDieren
                 .toLowerCase()
-        });
-        console.log(methods)
-    } catch (err) {
-        console.log(err)
-    }
+        })
+
+        // Fixing typos
+        .map((x) => {
+            return x
+                .replace('hont', 'hond')
+                .replace(/dwerg teckel/g, 'hond')
+                .replace('katten', 'kat')
+        })
+
+        // Emptying out unused words and numbers
+        .map((x) => {
+            return x
+                .replace(/-/g, '')
+                .replace('geen', '')
+                .replace(/'/g, '')
+                .replace('n>v>t>', '')
+                .replace(/[0-9]/g, '')
+        })
+
+        // Putting dots inbetween names and removing double and triple dots to have better clearance in the array
+        .map((x) => {
+            return x
+                .replace(/,/g, '.')
+                .replace(/ /g, '.')
+                .replace(/:/g, '.')
+        })
+
+        // Filtering out empty objects
+        .filter(x => !!x)
+
+        // Transform to strings and split the words
+        .toString().split('.')
+
+
+    // Filtering on animal names
+    const petsTotal = cleanPetsData.filter(name => animals.includes(name.toLowerCase()))
+
+    // Returning the mentioned pet names in an array
+    return petsTotal
 }
 
-getData(endpoint)
+console.log(cleanPets())
