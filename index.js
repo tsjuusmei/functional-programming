37a205caf41d603349ff26aaf596da17c0dd0cbc
tsjuusmei <guusmay2@hotmail.com>
2020-11-02 15:42:46 +0100

    got api data from api

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import cleanPets from './js/cleanuppets'
+import fetch from 'node-fetch'
 
-console.log(cleanPets())
+async function getData(endpoint) {
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+getData('https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000')
--- js/data.js
+++ js/data.js
@@ -1,1 +1,3 @@
- export const endpoint = 'https://opendata.rdw.nl/resource/r3rs-ibz5?$limit=1800'
+const endpoint = 'https://opendata.rdw.nl/resource/r3rs-ibz5.json?$limit=1800'
+
+export default endpoint
