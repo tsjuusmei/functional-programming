8eaf31296a519cf1962981a83cf687e81ed4c7d3
tsjuusmei <guusmay2@hotmail.com>
2020-11-11 23:17:26 +0100

    added comments, ordered the functions and edited gitignore

--- index.js
+++ index.js
@@ -30,15 +30,15 @@ async function getData(uriString) {
   return data
 }
 
-// This function
+// This function compares two endpoints, checks which shared key they have and returns them, also returns the data from the endpoints to work with later on
 function sharedIds(endpointA, endpointB, sharedKey) {
   const resultA = getData(endpointA)
   const resultB = getData(endpointB)
   let idsA = [];
   let sharedIds = [];
   return Promise.all([resultA, resultB]).then(result => {
-    idsA = result[0].map(x => x[sharedKey])
-    sharedIds = result[1].filter(x => idsA.includes(x[sharedKey])).map(x => x[sharedKey])
+    idsA = result[0].map(x => x[sharedKey]) // Here the keys will be mapped in an array
+    sharedIds = result[1].filter(x => idsA.includes(x[sharedKey])).map(x => x[sharedKey]) // Here the shared keys get filtered, compared and mapped out of the second result
     sharedIds = sharedIds.filter((item, pos) => { // This removes duplicate values from the array https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
       return sharedIds.indexOf(item) == pos;
     })
@@ -50,66 +50,33 @@ function sharedIds(endpointA, endpointB, sharedKey) {
   })
 }
 
+// This function filters out unused keys and combines the datasets together
 function filterData(data, keys) {
   let combinedData = [];
 
   data.forEach(dataset => {
     dataset.forEach(obj => {
       let cleanedObj = {};
-      const objInArray = combinedData.find(x => x.areaid === obj.areaid);
-
+      const objInArray = combinedData.find(x => x.areaid === obj.areaid); // Find shared areaids and puts them in a variable
       // areaid is already in, just add missing key/values
       if (objInArray) {
         cleanedObj = objInArray;
       }
-
+      // Sets the keys that we need in our dataset
       keys.forEach(key => {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
           // object has key, set it
           cleanedObj[key] = obj[key];
         }
       });
-
       combinedData = combinedData.filter(x => x.areaid !== obj.areaid);
       combinedData.push(cleanedObj);
-
     })
   });
   return combinedData;
 }
 
-function getCenterCord(coordinates) {
-  const type = coordinates.split(' ')[0]
-  let longLat = []
-  let coords = coordinates
-    .replace(type, '')
-    .replace(' ', '')
-    .replace(' (', '')
-    .replace('((', '')
-    .replace('))', '')
-    .replace('(', '')
-    .replace(')', '')
-    .replace(/,/g, '')
-    .split(' ')
 
-  if (coords.length > 2) {
-    let long = 0
-    let lat = 0
-
-    coords.forEach((coord, i) => {
-      if (i % 2 == 0) { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
-        return long += Number(coord)
-      }
-      return lat += Number(coord)
-    })
-
-    longLat = [long / (coords.length / 2), lat / (coords.length / 2)]
-
-  } else {
-    longLat = [Number(coords[0]), Number(coords[1])]
-  }
-  return longLat
-}
 
 // async function getUuid(areaid) {
 //   let uuid = undefined
@@ -118,14 +85,7 @@ function getCenterCord(coordinates) {
 //   return uuid
 // }
 
-async function mergeData() {
-  const result = await sharedIds(endpoints[0], endpoints[1], sharedKey);
-  let dataA = result.resultA.filter(x => result.sharedIds.includes(x.areaid));
-  let dataB = result.resultB.filter(x => result.sharedIds.includes(x.areaid));
-
-  return filterData([dataA, dataB], keys);
-}
-
+// This function checks what city a point coordinate is in based on polygons of cities
 function coordInPolygon(centerCoord, polygons) {
   let city = undefined
 
@@ -165,6 +125,8 @@ function coordInPolygon(centerCoord, polygons) {
 //   return tariffObj 
 // }
 
+
+// This function will retreive the tariffs for the facilities with the areaids
 function getTariffs(areaid) {
   const tariffObj = {}
   let getTariffs = allTariffs[areaid]
@@ -172,7 +134,7 @@ function getTariffs(areaid) {
     return undefined
   }
   Object.keys(getTariffs).forEach(key => {
-    tariffObj[key] = getTariffs[key].averageTariff * 1440
+    tariffObj[key] = getTariffs[key].averageTariff
   })
   return tariffObj
 }
@@ -200,6 +162,49 @@ function getTariffs(areaid) {
 //   return weightedTotalCharge / totalMinutes;
 // }
 
+// This function will put the coordinates in an array and calculate the middle coordinate of a polygon
+function getCenterCord(coordinates) {
+  const type = coordinates.split(' ')[0]
+  let longLat = []
+  let coords = coordinates // Clean the coordinates so we can use them
+    .replace(type, '')
+    .replace(' ', '')
+    .replace(' (', '')
+    .replace('((', '')
+    .replace('))', '')
+    .replace('(', '')
+    .replace(')', '')
+    .replace(/,/g, '')
+    .split(' ')
+
+  if (coords.length > 2) { // This code will run for polygons
+    let long = 0
+    let lat = 0
+
+    coords.forEach((coord, i) => {
+      if (i % 2 == 0) { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
+        return long += Number(coord)
+      }
+      return lat += Number(coord)
+    })
+
+    longLat = [long / (coords.length / 2), lat / (coords.length / 2)]
+
+  } else { // This code will run for points
+    longLat = [Number(coords[0]), Number(coords[1])] 
+  }
+  return longLat
+}
+
+// This function merges the datasets to 1 dataset
+async function mergeData() {
+  const result = await sharedIds(endpoints[0], endpoints[1], sharedKey);
+  let dataA = result.resultA.filter(x => result.sharedIds.includes(x.areaid));
+  let dataB = result.resultB.filter(x => result.sharedIds.includes(x.areaid));
+
+  return filterData([dataA, dataB], keys);
+}
+
 mergeData().then(x => {
   const newArray = x
     .map(y => {
@@ -225,6 +230,7 @@ mergeData().then(x => {
     //     })
     // })
 
+    // Get the tariffs
     .map(y => {
       let obj = y
       obj.tariffs = getTariffs(obj.areaid)
@@ -237,6 +243,7 @@ mergeData().then(x => {
     //   return obj
     // })
 
+    // Filters out empty tariffs
     .filter(y => {
       Object.size = function (obj) { // Checks the size of an object https://stackoverflow.com/questions/5223/length-of-a-javascript-object
         var size = 0, key;
@@ -250,8 +257,11 @@ mergeData().then(x => {
       }
       return y
     })
+    
+    // Filters out undefined cities
+    .filter(y =>  y.city)
 
   // Promise.all(newArray).then(x => console.log(x))
 
-  console.log(newArray)
+  // console.log(newArray)
 });
